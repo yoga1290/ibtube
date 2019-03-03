2fef177f6583ce28deafec565cf395c22db981b4
Youssef Gamil <yoga1290@gmail.com>
2019-03-03 20:03:01 +0200

    JSON support for TypeScript, Angular CLI and Karma Configuration

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/ibtube'),
+      dir: require('path').join(__dirname, '../docs/coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
