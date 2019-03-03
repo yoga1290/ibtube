09bb2a58b6170c45606e64ff3fe1e950665d5246
Youssef Gamil <yoga1290@gmail.com>
2019-03-03 22:13:59 +0200

    Playlist Preview UI + link fix

--- src/app/shared/playlist-preview/playlist-preview.component.ts
+++ src/app/shared/playlist-preview/playlist-preview.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Playlist } from '../../../services/models/Playlist';
-import * as moment from 'moment'
+import {
+  Playlist,
+  SearchResult,
+  PlaylistListResponse
+} from '@models';
+import { YoutubeService } from '@services/youtube.service'
+import { Router } from '@angular/router';
 
+import * as moment from 'moment'
 @Component({
   selector: 'app-playlist-preview',
   templateUrl: './playlist-preview.component.html',
@@ -9,13 +15,29 @@ import * as moment from 'moment'
 })
 export class PlaylistPreviewComponent implements OnInit {
 
-  @Input("playlist") playlist: Playlist = null;
+  @Input() playlist: Playlist = null;
+  @Input() searchResult: SearchResult = null;
 
-  constructor() { }  
+  constructor(
+            private router: Router,
+    private youtubeService: YoutubeService
+  ) { }  
   ngOnInit() {
+    // coming from a search query:
+    if (this.searchResult) {
+      let playlistId = this.searchResult.id.playlistId;
+      this.youtubeService
+      .getPlaylist(playlistId)
+      .subscribe((playlistListResponse : PlaylistListResponse) => {
+        this.playlist = playlistListResponse.items[0]
+      })
+    }
     console.log('PlaylistPreviewComponent', this.playlist)
   }
 
   fromNow = (isoDate: string) => (moment(isoDate).fromNow())
 
+  goToRoute(path: string) {
+    this.router.navigate([path]);
+  }
 }
