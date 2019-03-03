7306f364d7dcdf82be18a3aee1431b44658da84f
Youssef Gamil <yoga1290@gmail.com>
2019-03-03 19:36:11 +0200

    AppModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SearchComponent } from './search/search.component'
-const routes: Routes = [{
-  path: 'search', //?query
+import { SearchComponent } from './search/search.component';
+import { VideoComponent } from './video/video.component';
+import { ChannelComponent } from './channel/channel.component';
+import { environment } from '@env'
+
+
+let useHash = environment.useHash
+
+export const ROUTES: Routes = [{
+  path: 'search',
   component: SearchComponent
 }, {
   path: 'channel/:channelId',
-  component: SearchComponent
+  component: ChannelComponent
 }, {
   path: 'video/:videoId',
-  component: SearchComponent
+  component: VideoComponent
+}, {
+  path: '**',
+  redirectTo: '/search',
+  pathMatch: 'full'
 }];
 
 @NgModule({
   // useHash for a backend-free app (no need for ng serve)
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(ROUTES, { useHash })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {MaterialModule} from './material.module'
+// import {MaterialModule} from './material.module';
+
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { VideoComponent } from './video/video.component';
+import { VideoPreviewComponent } from './shared/video-preview/video-preview.component';
+import { ChannelComponent } from './channel/channel.component';
+import { PlaylistPreviewComponent } from './shared/playlist-preview/playlist-preview.component'
 
-import { HttpClientModule }    from '@angular/common/http';
-import providers from '@services'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import services from '@services';
+import { HttpInterceptorConfig } from './HttpInterceptorConfig';
+
+let providers: Provider[] = services;
+providers.push({
+  multi: true,
+  provide: HTTP_INTERCEPTORS,
+  useClass: HttpInterceptorConfig
+});
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    VideoComponent,
+    VideoPreviewComponent,
+    ChannelComponent,
+    PlaylistPreviewComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
+    // MaterialModule, //no material now; just SCSS :)
     HttpClientModule,
     AppRoutingModule
   ],
-  providers,//: [],
+  providers,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
